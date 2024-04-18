ba1bac27dc313b8456c2b524cfcef439c19d25e8
dev4393 <ds.rajpoot@nic.in>
2024-04-18 14:08:39 +0530

    added daisy ui library

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,12 @@
+import Carousel from "./comonents/Carousel"
+import Navbar from "./comonents/Navbar"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="font-bold text-3xl">
+      <Navbar />
+      <Carousel />
+    </div>
   )
 }
 
---
+++ Client/src/comonents/Carousel.jsx
@@ -0,0 +1,23 @@
+import banner1 from '../assets/banner1.webp'
+import banner2 from '../assets/banner2.webp'
+import banner3 from '../assets/banner3.webp'
+const Carousel = () => {
+  return (
+    <>
+    <div className="carousel carousel-end rounded-box ">
+  <div className="carousel-item">
+    <img src={banner1} alt="Drink" />
+  </div> 
+  <div className="carousel-item">
+    <img src={banner2} alt="Drink" />
+  </div> 
+  <div className="carousel-item">
+    <img src={banner3} alt="Drink" />
+  </div> 
+  
+</div>
+    </>
+  )
+}
+
+export default Carousel
