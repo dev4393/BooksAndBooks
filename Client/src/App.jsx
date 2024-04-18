96df78233933910195788d95e596cf5537ec5f6f
dev4393 <ds.rajpoot@nic.in>
2024-04-18 16:03:16 +0530

    routing added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,22 @@
+import About from "./comonents/About";
 import Carousel from "./comonents/Carousel"
+import Contact from "./comonents/Contact";
+import Home from "./comonents/Home";
 import Navbar from "./comonents/Navbar"
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <div className="font-bold text-3xl">
+    <div className="text-3xl font-bold">
+
+      <BrowserRouter>
       <Navbar />
       <Carousel />
+      <Routes>
+      <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   )
 }
--- Client/src/comonents/Navbar.jsx
+++ Client/src/comonents/Navbar.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
@@ -6,23 +6,23 @@ const Navbar = () => {
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Home</a></li>
+        <li><Link to="/">Home</Link></li>
         <li>
-          <a>About</a>
+        <Link to="/about">About</Link>
         </li>
-        <li><a>Contact Us</a></li>
+        <li><Link to="/contact">Contact Us</Link></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">Sveep Mungeli</a>
+    <a className="text-2xl font-bold btn btn-ghost">Books And Books</a>
   </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Home</a></li>
-      <li><a>About </a></li>
-      <li><a>Contact Us</a></li>
+  <div className="hidden navbar-center lg:flex">
+    <ul className="px-1 menu menu-horizontal">
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/contact">Contact Us</Link></li>
     </ul>
   </div>
   <div className="navbar-end">
