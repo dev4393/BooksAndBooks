5827b691718de5dbf3d887206fe80af015c9af94
dev4393 <ds.rajpoot@nic.in>
2024-05-21 15:45:08 +0530

    Tanstack Query

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,12 +3,21 @@ import Contact from "./comonents/Contact";
 import Courses from "./comonents/Courses";
 import Footer from "./comonents/Footer";
 import Home from "./comonents/Home";
-import Navbar from "./comonents/Navbar"
+import Navbar from "./comonents/Navbar";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions:{}
+})
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <div >
-
+   <QueryClientProvider client={queryClient}>
       <BrowserRouter>
       <Navbar />
       <hr className="mb-36" />
@@ -20,6 +29,8 @@ const App = () => {
       </Routes>
       <Footer />
     </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
     </div>
   )
 }
--- Client/src/comonents/Courses.jsx
+++ Client/src/comonents/Courses.jsx
@@ -1,7 +1,39 @@
-const Courses = () => {
-  return (
-    <div>Courses</div>
-  )
-}
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-export default Courses
+// Add <QueryClientProvider client={queryClient}> in app.jsx 
+//Use useQuery fn to get data provide key and fn(which gets the data using axios) to it in object
+//useQuery has data ,isLoading,isError,error parameters
+const baseUrl = "https://jsonplaceholder.typicode.com";
+const getBlogs = async () => {
+  try {
+    const { data } = await axios.get(`${baseUrl}/posts`);
+    return data;
+  } catch (error) {
+    throw new Error("TestError");
+  }
+};
+
+const Courses = () => {
+  const {data,isLoading,isError,error} = useQuery({
+    queryKey: ["BLOGS"],
+    queryFn: getBlogs,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error.message}</div>;
+  return (
+    <div>
+      <h1>Courses</h1>
+      <ul>
+        {data.map((blog) => (
+          <li key={blog.id}>
+            {blog.id}::{blog.body}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Courses;
